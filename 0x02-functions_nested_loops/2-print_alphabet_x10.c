87768826bf82fdf99702fe4dea2d120b7ec39c60
BONKOUNGOU Benewende Pierre <90104700+B6WP2@users.noreply.github.com>
2022-09-14 21:45:03 +0000

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,12 @@
 
 /**
  * main - check the code.
- *int n, co;
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n, co;
 
 	co = 0;
 
@@ -12,13 +17,10 @@
 		for (n = 'a'; n <= 'z'; n++)
 		{
 			_putchar(n);
-		}
+	}
 		co++;
 		_putchar('\n');
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet_x10();
-    return (0);
+		
+         print_alphabet_x10();
+         return (0);
 }
