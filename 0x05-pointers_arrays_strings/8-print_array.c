a33842b6dda1b3823e64ff90af279001bddd84b1
BONKOUNGOU Benewende Pierre <90104700+B6WP2@users.noreply.github.com>
2022-09-19 12:03:15 +0000

    Arrays are not pointers
    Write a function that prints n elements of an array of integers, followed by a new line.
    
    Prototype: void print_array(int *a, int n);
    where n is the number of elements of the array to be printed
    Numbers must be separated by comma, followed by a space
    The numbers should be displayed in the same order as they are stored in the array
    You are allowed to use printf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,14 @@
 #include <stdio.h>                                                 
 #include <stdlib.h>
 #include "main.h"
+
 /**
 *print_array - print n element of array
 *@a: array
 *@n: number of elements
 *Return: nothing
 */
+
 void print_array(int *a, int n)
 {
 int inc;
