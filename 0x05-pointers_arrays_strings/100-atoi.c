acfb8a46263d50ba93577d19ad7530a9eb5bf17c
BONKOUNGOU Benewende Pierre <90104700+B6WP2@users.noreply.github.com>
2022-09-19 12:05:19 +0000

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers
    Write a function that convert a string to an integer.
    
    Prototype: int _atoi(char *s);
    The number in the string can be preceded by an infinite number of characters
    You need to take into account all the - and + signs before the number
    If there are no numbers in the string, the function must return 0
    You are not allowed to use long
    You are not allowed to declare new variables of “type” array
    You are not allowed to hard-code special values
    We will use the -fsanitize=signed-integer-overflow gcc flag to compile your code.
    FYI: The standard library provides a similar function: atoi. Run man atoi to learn more.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,14 @@
 #include <stdio.h>                                                 
 #include <stdlib.h>                                                
 #include <main.h>  
+
 /**
 * _atoi - changes a string to an int
 * @s: the string to be changed
 *
 * Return: the converted int
 */
+
 int _atoi(char *s)
 {
 int i = 1;
