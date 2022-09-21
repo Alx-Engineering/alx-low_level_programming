ed7086e6910c1ed4a2a3e93053758bf980ba9286
BONKOUNGOU Benewende Pierre <90104700+B6WP2@users.noreply.github.com>
2022-09-21 12:06:45 +0000

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,16 +10,16 @@
 
 int main(void)
 {
-char c;
-int x;
+  char c;
+  int x;
 
-srand(main(0));
-while (x <= 2645)
-{
-c = rand() % 128;
-x += c;
-putchar(c);
-}
-putchar(2772 - x);
-return (0);
+  srand(main(0));
+  while (x <= 2645)
+  {
+     c = rand() % 128;
+     x += c;
+     putchar(c);
+  }
+  putchar(2772 - x);
+  return (0);
 }
