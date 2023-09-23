4356ec38bc92ea6180c38811e77231ad5e4fb4de
B P BONKOUNGOU <benepeter.b@gmail.com>
2023-09-23 04:11:42 -0700

    Task 7: Update 7-puts_half.c and 7-main.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * puts_half - Prints half of a string followed by a new line.
@@ -6,22 +7,22 @@
  */
 void puts_half(char *str)
 {
-int length = 0;
-int i;
+    int length = 0;
+    int i;
 
-while (str[length])
-length++;
+    while (str[length])
+        length++;
 
-if (length % 2 == 0)
-i = length / 2;
-else
-i = (length - 1) / 2;
+    if (length % 2 == 0)
+        i = length / 2;
+    else
+        i = (length + 1) / 2;
 
-while (str[i])
-{
-_putchar(str[i]);
-i++;
-}
+    while (str[i])
+    {
+        _putchar(str[i]);
+        i++;
+    }
 
-_putchar('\n');
+    _putchar('\n');
 }
