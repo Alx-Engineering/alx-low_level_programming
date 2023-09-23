defb89c3d043dc1327ac3b1f2dc45f321b63912b
B P BONKOUNGOU <benepeter.b@gmail.com>
2023-09-23 02:36:09 -0700

    Task 0: Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,10 @@
-#include <stdio.h>                                                 
-#include <stdlib.h> 
 #include "main.h"
 
 /**
-*reset_to_98 - reset value of an integer to 98
-*@n: parameter targeted
-*Return : nothing
-*/
-
+ * reset_to_98 - Updates the value it points to to 98.
+ * @n: A pointer to an integer.
+ */
 void reset_to_98(int *n)
 {
-*n = 98;
+    *n = 98;
 }
