3b87b06d4b22ecfd152d28108bd6d2b2f18280f3
B P BONKOUNGOU <benepeter.b@gmail.com>
2023-09-23 03:42:47 -0700

    Task 100: Update 100-atoi.c and 100-main.c

---
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "-123456789";
+    int number;
+
+    number = _atoi(s);
+    printf("Number: %d\n", number);
+    return (0);
+}
