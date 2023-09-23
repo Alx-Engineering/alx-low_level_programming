ae3d0b87ef717fdcfaee6c4354b535feb5c58646
B P BONKOUNGOU <benepeter.b@gmail.com>
2023-09-23 03:15:01 -0700

    Task 1: Update 1-swap.c et 1-main.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,15 @@
-#include <stdio.h>                                                 
-#include <stdlib.h> 
 #include "main.h"
 
 /**
-*swap_int - swap two integers
-*@a: first integer
-*@b: second integer
-*Return: nothing
-*/
-
+ * swap_int - Swaps the values of two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ */
 void swap_int(int *a, int *b)
 {
-int tmp = *a;
-*a = *b;
-*b = tmp;
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
