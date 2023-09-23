dc9ba3452601250bd9647a1a094f764a7f43ec76
B P BONKOUNGOU <benepeter.b@gmail.com>
2023-09-23 03:42:45 -0700

    Task 9: Update 9-strcpy.c and 9-main.c

---
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *src = "Hello, Holberton!";
+    char dest[50];
+
+    _strcpy(dest, src);
+    printf("Destination string: %s\n", dest);
+    return (0);
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,22 @@
-#include <stdio.h>                                                 
-#include <stdlib.h> 
 #include "main.h"
 
 /**
-* _strcpy - Copy paste string
-*@dest: destination
-*@src: source
-*Return: dest
-*/
-
+ * _strcpy - Copies a string to a buffer.
+ * @dest: The buffer to copy to.
+ * @src: The string to copy.
+ *
+ * Return: The pointer to dest.
+ */
 char *_strcpy(char *dest, char *src)
 {
-int inc = 0;
-while (*(src + inc) != '\0')
-{
-*(dest + inc) = *(src + inc);
-inc++;
-}
-*(dest + inc) = '\0';
-return (dest);
+    int i = 0;
+
+    while (src[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+
+    dest[i] = '\0';
+    return (dest);
 }
